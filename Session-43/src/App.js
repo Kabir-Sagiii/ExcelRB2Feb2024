f1d609365e43fa4f3bb0ce14313a572b1f1eae01
Kabir <sagithedeveloper@gmail.com>
2024-05-14 22:25:59 +0530

    added the code

---
+++ Session-43/src/App.js
@@ -0,0 +1,17 @@
+import List from "./List";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to React JS</h2>
+
+      <List />
+
+      <List />
+
+      {List()}
+    </div>
+  );
+}
+
+export default App;
