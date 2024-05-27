2f3e1469665f35cfb5a691cd5d81a50d028838e7
Kabir <sagithedeveloper@gmail.com>
2024-05-27 22:15:43 +0530

    added the code

---
+++ Session-49/src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./components/nav/Nav";
+import Users from "./components/Users/Users";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Users />
+    </div>
+  );
+}
+
+export default App;
